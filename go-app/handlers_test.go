bc56a1fbdd14a54999ab23a458e1f0d9cf5075eb
Tomas Šličius <slctomas99@gmail.com>
2025-01-22 15:13:15 +0200

    testhandlegetfilebyid

--- go-app/handlers_test.go
+++ go-app/handlers_test.go
@@ -61,7 +61,13 @@ func TestHandleFileUpload(t *testing.T) {
 	r.ServeHTTP(rr, req)
 
 	if rr.Code != http.StatusOK {
-		t.Error("GET to '/upload' -> expected code 200, got code", rr.Code)
+		t.Error("POST to '/upload' -> expected code 200, got code", rr.Code)
+	}
+
+	// Check response body
+	expected := "File uploaded successfully"
+	if !bytes.Contains(rr.Body.Bytes(), []byte(expected)) {
+		t.Errorf("Expected response body to contain %q, got %q", expected, rr.Body.String())
 	}
 }
 
@@ -88,5 +94,39 @@ func TestHandleGetAllFiles(t *testing.T) {
 
 	if rr.Code != http.StatusOK {
 		t.Error("GET to '/files' -> expected code 200, got code", rr.Code)
+	}
+}
+
+func TestHandleGetFileByID(t *testing.T) {
+	app := &Config{
+		UploadDir:         uploadDir,
+		MaxFileSize:       maxFileSize,
+		AllowedExtensions: allowedExtensions,
+		Uploads:           []fileUpload{{ID: 1, Filename: "testimage.png"}},
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/files/{fileID}", app.handleGetFileByID)
+	app.assignIDs()
+
+	req, err := http.NewRequest(http.MethodGet, lc+"/files/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	r.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Error("GET to '/files/1' -> expected code 200, got code", rr.Code)
+	}
+
+	// Check response body
+	expected := "file found"
+	if !bytes.Contains(rr.Body.Bytes(), []byte(expected)) {
+		t.Errorf("Expected response body to contain %q, got %q", expected, rr.Body.String())
 	}
 }
